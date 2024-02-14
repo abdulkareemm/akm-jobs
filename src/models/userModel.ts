7d8043b24396c852f1f700ac0b27f9c9e6225476
abdulkareemm <eng.kreem.96@gmail.com>
2024-02-14 09:17:53 +0300

    create user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: "string",
+    required: true,
+  },
+  email: {
+    type: "string",
+    required: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+  },
+  isAdmin:{
+    type: "boolean",
+    required: true,
+    default: false,
+  }
+},{
+    timestamps:true
+});
+
+
+// delete old one
+const userModel = mongoose.model('user')
+mongoose.deleteModel(userModel.modelName)
+
+// create new model
+module.exports =mongoose.model('user',userSchema)
