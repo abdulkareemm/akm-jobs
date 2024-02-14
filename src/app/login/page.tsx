15037eb1d80c6684c2c52b3f26cb2dd87c01f27f
abdulkareemm <eng.kreem.96@gmail.com>
2024-02-14 08:38:04 +0300

    design authentication pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,11 @@
 "use client";
-import { Button, Form } from "antd";
+import { Button, Form, Radio } from "antd";
 import Link from "next/link";
 import React from "react";
 
 const Login = () => {
 
-  const onFinsh = (values:any)=>{
-    
-  }
+  const onFinish = (values: any) => {};
   return (
     <div className="flex justify-center h-screen items-center bg-primary">
       <div className="card p-5 w-450">
@@ -16,8 +14,14 @@ const Login = () => {
         <Form
           layout="vertical"
           className="flex flex-col gap-3"
-          onFinish={onFinsh}
+          onFinish={onFinish}
         >
+          <Form.Item label="Login as " name="userType">
+            <Radio.Group>
+              <Radio value="employer">Employer</Radio>
+              <Radio value="employee">Employee</Radio>
+            </Radio.Group>
+          </Form.Item>
           <Form.Item label="Email" name="email">
             <input type="email" className="input" />
           </Form.Item>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,49 @@
+"use client"
+import { Button, Form, Radio } from 'antd';
+import Link from 'next/link';
 import React from 'react'
 
 const Register = () => {
+  const onFinish = (values : any)=>{
+
+  }
   return (
-    <div>Register</div>
-  )
+    <div className="flex justify-center h-screen items-center bg-primary">
+      <div className="card p-5 w-450">
+        <h1 className="text-xl ">A K M JOPS Register</h1>
+        <hr />
+        <Form
+          layout="vertical"
+          className="flex flex-col gap-3"
+          onFinish={onFinish}
+        >
+          <Form.Item label="Register as " name="userType">
+            <Radio.Group>
+              <Radio value="employer">Employer</Radio>
+              <Radio value="employee">Employee</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label="Username" name="username">
+            <input type="text" className="input" />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <input type="email" className="input" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <input type="password" className="input" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit" block>
+            Login
+          </Button>
+          <Link href="/login">
+            <span className="mt-5">
+              Do you have an account? <span>Login</span>
+            </span>
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Register
