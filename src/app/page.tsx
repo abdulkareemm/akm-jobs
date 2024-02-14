056b23e0be7d9f0758debbd8c4a8809844ba6398
abdulkareemm <eng.kreem.96@gmail.com>
2024-02-14 14:28:39 +0300

    remove get user from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,10 @@
 "use client";
-import { Button, message } from "antd";
-import axios from "axios";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [user, setUser] = useState(null);
-  const getUser = async () => {
-    try {
-      const response = await axios.get("/api/users/currentuser");
-      setUser(response.data.data);
-    } catch (error: any) {
-      message.error(error.response.data.message || error.message);
-    }
-  };
-  useEffect(() => {
-    getUser();
-  }, []);
+  
   return (
     <main>
-      <Button type="primary"> Pr</Button>
+      home
     </main>
   );
 }
