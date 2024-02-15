c3014b185186bd832369e40aaae366f3ca247079
abdulkareemm <eng.kreem.96@gmail.com>
2024-02-15 21:06:27 +0300

    create application model

---
+++ src/models/applicationModel.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "jobs",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "shortlisted", "rejected"],
+    },
+  },
+  { timestamps: true }
+);
+
+// delete old model
+if (mongoose.models.applications) {
+  const applicationModel = mongoose.model("applications");
+  mongoose.deleteModel(applicationModel.modelName);
+}
