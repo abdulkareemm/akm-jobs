0b4606705d95547d5c218810070427fe497a210d
abdulkareemm <eng.kreem.96@gmail.com>
2024-02-15 12:48:01 +0300

    add some fields to user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -24,6 +24,43 @@ const userSchema = new mongoose.Schema(
       required: true,
       default: "employee",
     },
+    // additional fields for employee
+    skills: {
+      type: [],
+      required: false,
+    },
+    experience: {
+      type: [],
+      required: false,
+    },
+    education: {
+      type: [],
+      required: false,
+    },
+    carrierObjective: {
+      type: String,
+      required: false,
+    }, // additional fields for employer
+    establishmentYear: {
+      type: String,
+      required: false,
+    },
+    companySize: {
+      type: String,
+      required: false,
+    },
+    website: {
+      type: String,
+      required: false,
+    },
+    about: {
+      type: String,
+      required: false,
+    },
+    address: {
+      type: String,
+      required: false,
+    },
   },
   {
     timestamps: true,
@@ -31,11 +68,10 @@ const userSchema = new mongoose.Schema(
 );
 
 // delete old one
-if(mongoose.models.users){
+if (mongoose.models.users) {
   const userModel = mongoose.model("users");
   mongoose.deleteModel(userModel.modelName);
 }
-
 
 const User = mongoose.model("users", userSchema);
 export default User;
